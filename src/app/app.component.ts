4856a44065ae2f5996b37c5bb53badd420596722
leiner.barrios@inglesparatodos.edu.co <devappipt@Devs-iMac.local>
2022-07-25 14:02:17 -0500

    Header and history button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rick-and-morty-app';
+
+  items!: MenuItem[];
+
+  ngOnInit() {
+    this.items = [{
+        label: 'Options',
+        items: [{
+            label: 'Update',
+            icon: 'pi pi-refresh',
+        },
+        {
+            label: 'Delete',
+            icon: 'pi pi-times',
+        }
+        ]},
+        {
+            label: 'Navigate',
+            items: [{
+                label: 'Angular',
+                icon: 'pi pi-external-link',
+                url: 'http://angular.io'
+            },
+            {
+                label: 'Router',
+                icon: 'pi pi-upload',
+                routerLink: '/fileupload'
+            }
+        ]}
+    ];
+}
 }
--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import {ButtonModule} from 'primeng/button';
-
-
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ButtonModule
+    ButtonModule,
+    MenuModule
   ],
   exports: [
-    ButtonModule
-  ]
+    ButtonModule,
+    MenuModule
+  ],
 })
 export class PrimeNgModule { }
